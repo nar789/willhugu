3fbbcdfb6bc48ff729c0c7b42b8116f8ff4d1ee3
nar789 <admin@hume.co.kr>
2020-02-11 03:22:07 +0900

    launching

--- src/Route.js
+++ src/Route.js
@@ -3,11 +3,55 @@
 
 		var app = _g.app;
 		var http = _g.http;
+		var mysql = _g.mysql;
+		/*
+		var config = {
+			host     : 'localhost',
+			user     : 'root',
+			password : 'apmsetup',
+			database : 'willhugu'
+		}*///debug
+
+
+		var config = {
+			host     : 'localhost',
+			user     : 'root',
+			password : 'l323585@',
+			database : 'willhugu'
+		}
+
+
+		function qry(qry,callback){
+			var conn = mysql.createConnection(config);
+			conn.connect();
+			conn.query(qry, function (error, results, fields) {
+				if (error) {
+					console.log(error);
+				}
+				if(callback!=null){
+					callback(results);
+				}
+				
+			});
+			conn.end();
+		}
+
 
 		function route(){
 			app.get('/',function(req,res){
 				loginCheckRouteHook(()=>{
 					res.render('index.html',{});
+				});
+			});
+
+			app.post('/apply',function(req,res){
+				var name = encodeURIComponent(req.body.name);
+				var email = req.body.email;
+				var phone = req.body.phone;
+				var m = encodeURIComponent(req.body.m);
+				var q = `insert into apply values(null,'${name}','${email}','${phone}','${m}')`;
+				qry(q,(ret)=>{
+					res.send('success');
 				});
 			});
 
